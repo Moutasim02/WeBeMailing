be13cdc565bfed2fc9b86c63e32dd49d019cd716
Moutasim El Ayoubi <moutasim9997@gmail.com>
2022-11-19 07:06:40 +0400

    Update

--- networking.js
+++ networking.js
@@ -1,5 +1,5 @@
 // Imports
-import axios from 'axios'
+const axios = require('axios')
 
 // API key
 const API_KEY = "JSRUKTOXTRCFBOL1K0ZFC"; // Get your API key here: https://app.rytr.me/account/api-access
